fd049d93e5571538d91bdeb76d22d56c6e11d1b0
Shnupbups <shnupbups@gmail.com>
2019-03-29 01:15:38 +1100

    Version 1.1.0
    JSON, JSON and more JSON.
    Changelog:
    - Updated to 19w13a
    - Added Stairs, Slabs, Sidings and Walls for... a lot of things.
    - I mean a lot of things.
    - I mean, this mod adds more recipes than the entire vanilla game has.
    - A lot of things.

---
+++ src/main/java/com/shnupbups/extrapieces/StairsBlock.java
@@ -0,0 +1,11 @@
+package com.shnupbups.extrapieces;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+
+public class StairsBlock extends net.minecraft.block.StairsBlock {
+
+	public StairsBlock(BlockState state, Block.Settings settings) {
+		super(state, settings);
+	}
+}
